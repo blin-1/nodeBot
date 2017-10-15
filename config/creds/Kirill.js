65b728bb9be06d1a0b213f356655ab3da3f92546
blin-1 <y.monsoon@gmail.com>
2017-10-15 14:10:42 -0400

    Creds

--- config/creds/Kirill.js
+++ config/creds/Kirill.js
@@ -2,5 +2,6 @@
 //let Creds = {email:'v.osh2003@gmail.com',password:'Botik123'};// Popugaj // убит походу
 //let Creds = {email:'vosh2003@gmail.com',password:'Botik456'};// Gamadril
 //let Creds = {email:'vo.sh2003@gmail.com',password:'Botik123'};// Yuhnovich
-let Creds = {email:'vos.h2003@gmail.com',password:'Botik123'};// КхеКхе
+//let Creds = {email:'vos.h2003@gmail.com',password:'Botik123'};// КхеКхе
+let Creds = {email:'v.o.sh2003@gmail.com',password:'Botik123'};// Popugaj
 module.exports = Creds;
